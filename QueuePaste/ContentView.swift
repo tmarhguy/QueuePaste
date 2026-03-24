64fa50b0c498550c1b00cd41808297f3055124ff
Tyrone Marhguy <tmarhguy@seas.upenn.edu>
2026-03-24 16:01:34 -0400

    Initial Commit

---
+++ QueuePaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QueuePaste
+//
+//  Created by Tyrone Marhguy on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QueuePaste/QueuePasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueuePasteApp.swift
+//  QueuePaste
+//
+//  Created by Tyrone Marhguy on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct QueuePasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
